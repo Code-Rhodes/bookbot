3a6cf0ba6cce9ed583d28e19660526b9c92cb194
Code-Rhodes <hamorjairos@gmail.com>
2025-08-27 01:29:23 +0800

    stats.py

--- stats.py
+++ stats.py
@@ -2,6 +2,18 @@ def get_num_words(text):
     words = text.split()
     return len(words)
 
+#U3 - shows top 10 words
+def get_top_words(text, top_n=10):
+    words = text.lower().split()
+    word_counts = {}
+    for word in words:
+        if word in word_counts:
+            word_counts[word] += 1
+        else:
+            word_counts[word] = 1
+    sorted_words = sorted(word_counts.items(), key=lambda x: x[1], reverse=True)
+    return sorted_words[:top_n], sum(word_counts.values())
+
 def get_lower_count(text):
     text = text.lower()
     lower = {}
